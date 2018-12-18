85f23369211cd43cf46d968fc61f1b98c406f464
jpersson <shua74@charter.net>
2018-12-17 21:57:56 -0500

    Made minor updates.

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -6,6 +6,6 @@
 
 @section('content')
 
-    <p> Welcome to the WSP Geospatial Project Tracker. Please use the links above to either enter new project information or to search for project information.</p>
+    <h2> Welcome to the WSP Geospatial Project Tracker. Please use the links above to either enter new project information or to search for project information.</h2>
 
 @endsection
